3a5e0b443086b2f38f5198d06041db9f430ddf54
Eddie Atkinson <eddiebatkinson@gmail.com>
2019-10-17 17:35:03 -0400

    more css

--- js/scripts.js
+++ js/scripts.js
@@ -1,6 +1,45 @@
+let left1 = 0;
+let top1 = 0;
+let left2 = 0;
+let top2 = 0;
+let left3 = 0;
+let top3 = 0;
 $(document).ready(() => {
   console.log('Ready!');
   $('.button-1-up').click(() => {
-    $('#image1').html('button-1-up!');
+    $('#image1').css("top", `${top1-1}`);
+  });
+  $('.button-1-down').click(() => {
+    
+  });
+  $('.button-1-left').click(() => {
+    $('#image1').css("")
+  });
+  $('.button-1-right').click(() => {
+    
+  });
+  $('.button-2-up').click(() => {
+    
+  });
+  $('.button-2-down').click(() => {
+    
+  });
+  $('.button-2-left').click(() => {
+    $('#image2').css("")
+  });
+  $('.button-2-right').click(() => {
+    
+  });
+  $('.button-3-up').click(() => {
+    
+  });
+  $('.button-3-down').click(() => {
+    
+  });
+  $('.button-3-left').click(() => {
+    $('#image3').css("")
+  });
+  $('.button-3-right').click(() => {
+    
   });
 });
