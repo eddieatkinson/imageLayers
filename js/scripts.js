d64e4d5a701cb73497c423d895417923aa3104fc
Eddie Atkinson <eddiebatkinson@gmail.com>
2019-10-20 10:45:04 -0400

    move all three images

--- js/scripts.js
+++ js/scripts.js
@@ -4,43 +4,57 @@ let left2 = 0;
 let top2 = 0;
 let left3 = 0;
 let top3 = 0;
-$(document).ready(() => {
+$(document).ready((params) => {
   console.log('Ready!');
+  // image1
   $('.button-1-up').click(() => {
-    $('#image1').css("top", `${top1-1}px`);
     top1--;
+    $('#image1').css("top", `${top1}px`);
   });
   $('.button-1-down').click(() => {
-    
+    top1++;
+    $('#image1').css("top", `${top1}px`);
   });
   $('.button-1-left').click(() => {
-    $('#image1').css("")
+    left1--;
+    $('#image1').css("left", `${left1}px`);
   });
   $('.button-1-right').click(() => {
-    
+    left1++;
+    $('#image1').css("left", `${left1}px`);
   });
+  // image2
   $('.button-2-up').click(() => {
-    
+    top2--;
+    $('#image2').css("top", `${top2}px`);
   });
   $('.button-2-down').click(() => {
-    
+    top2++;
+    $('#image2').css("top", `${top2}px`);
   });
   $('.button-2-left').click(() => {
-    $('#image2').css("")
+    left2--;
+    $('#image2').css("left", `${left2}px`);
   });
   $('.button-2-right').click(() => {
-    
+    left2++;
+    $('#image2').css("left", `${left2}px`);
   });
+  // image3
   $('.button-3-up').click(() => {
-    
+    top3--;
+    $('#image3').css("top", `${top3}px`);
   });
   $('.button-3-down').click(() => {
-    
+    top3++;
+    $('#image3').css("top", `${top3}px`);
   });
   $('.button-3-left').click(() => {
-    $('#image3').css("")
+    left3--;
+    $('#image3').css("left", `${left3}px`);
   });
   $('.button-3-right').click(() => {
-    
+    left3++;
+    $('#image3').css("left", `${left3}px`);
   });
 });
